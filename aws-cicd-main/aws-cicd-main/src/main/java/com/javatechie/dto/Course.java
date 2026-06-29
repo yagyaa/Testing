0345340bb234b811b59db638ea91f62c0bd4d809
yagyaa <yagyaa87@gmail.com>
2026-06-29 20:07:25 +0530

    Update Course.java

--- aws-cicd-main/aws-cicd-main/src/main/java/com/javatechie/dto/Course.java
+++ aws-cicd-main/aws-cicd-main/src/main/java/com/javatechie/dto/Course.java
@@ -1,15 +1,41 @@
 package com.javatechie.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Course {
 
     private int id;
     private String name;
     private double price;
+
+    public Course() {
+    }
+
+    public Course(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
